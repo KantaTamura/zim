55b50349e23e8262971a1197be926b8faee73d75
KantaTamura <tkanta496@gmail.com>
2023-01-14 16:50:44 +0900

    :sparkles: chapter1 - reverse terminal attribute

--- src/main.zig
+++ src/main.zig
@@ -2,19 +2,30 @@ const std = @import("std");
 const io = std.io;
 const os = std.os;
 
+// default terminal attribute
+var original_termios : os.termios = undefined;
+const stdin_handle = io.getStdIn().handle;
+
 pub fn main() !void {
+    defer disableRawMode();
     try enableRawMode();
     const stdin = io.getStdIn().reader();
     var c: [1]u8 = undefined;
     while (true) {
         var c_size = try stdin.read(&c);
         if (c_size != 1 or c[0] == 'q') break;
-    }   
+    }
 }
 
 fn enableRawMode() !void {
-    const handler = io.getStdIn().handle;
-    var raw = try os.tcgetattr(handler);
+    original_termios = try os.tcgetattr(stdin_handle);
+    var raw = original_termios;
     raw.lflag &= ~(os.linux.ECHO);
-    try os.tcsetattr(handler, os.TCSA.FLUSH, raw);
+    try os.tcsetattr(stdin_handle, os.TCSA.FLUSH, raw);
+}
+
+fn disableRawMode() void {
+    os.tcsetattr(stdin_handle, os.TCSA.FLUSH, original_termios) catch |err| {
+        std.debug.print("{}", .{err});
+    };
 }

03acd0e9dd1213a20364040a1af9ff2e7bcbcd54
KantaTamura <tkanta496@gmail.com>
2023-01-14 16:12:43 +0900

    :sparkles: chapter1 - change terminal mode

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,18 @@
 const std = @import("std");
 const io = std.io;
+const os = std.os;
 
 pub fn main() !void {
+    try enableRawMode();
     const stdin = io.getStdIn().reader();
     var c: [1]u8 = undefined;
     while (true) 
         if (try stdin.read(&c) != 1 or c[0] == 'q') break;
+}
+
+fn enableRawMode() !void {
+    const handler = io.getStdIn().handle;
+    var raw = try os.tcgetattr(handler);
+    raw.lflag &= ~@as(os.tcflag_t, os.linux.ECHO);
+    try os.tcsetattr(handler, os.TCSA.FLUSH, raw);
 }

8e7458388571249fd7bb5d2caf6b16d15b663f6d
KantaTamura <tkanta496@gmail.com>
2023-01-15 21:24:59 +0900

    :sparkles: chapter3 - title

--- src/Editor.zig
+++ src/Editor.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const io = std.io;
 const os = std.os;
+const fmt = std.fmt;
 const debug = std.debug;
 const Terminal = @import("Terminal.zig");
 const AppendBuffer = @import("AppendBuffer.zig");
@@ -12,11 +13,13 @@ const Self = @This();
 
 ter: Terminal,
 buf: AppendBuffer,
+version: []const u8,
 
-pub fn init(ter: Terminal, buf: AppendBuffer) Self {
+pub fn init(ter: Terminal, buf: AppendBuffer, version: []const u8) Self {
     return Self{
         .ter = ter,
         .buf = buf,
+        .version = version,
     };
 }
 
@@ -47,17 +50,34 @@ fn ctrlKey(k: u8) u8 {
 }
 
 pub fn refreshScreen(self: *Self) !void {
-    try self.buf.append("\x1b[2J");
+    try self.buf.append("\x1b[?25l");
     try self.buf.append("\x1b[H");
     try self.drawRows();
     try self.buf.append("\x1b[H");
+    try self.buf.append("\x1b[?25h");
     try stdout.writeAll(self.buf.memory.items);
 }
 
 fn drawRows(self: *Self) !void {
     var y: usize = 0;
     while (y < self.ter.size.row) : (y += 1) {
-        try self.buf.append("~");
+        if (y == @divTrunc(self.ter.size.row, 3)) {
+            var welcome: [27]u8 = undefined;
+            var fis = io.fixedBufferStream(&welcome);
+            var writer = fis.writer();
+            try fmt.format(writer, "zim editor -- version {s}", .{self.version});
+            var padding = @divTrunc((@intCast(usize, self.ter.size.col) -| welcome.len), 2);
+            if (padding > 0) {
+                try self.buf.append("~");
+                padding -= 1;
+            }
+            while (padding > 0) : (padding -= 1)
+                try self.buf.append(" ");
+            try self.buf.append(&welcome);
+        } else {
+            try self.buf.append("~");
+        }
+        try self.buf.append("\x1b[K");
         if (y < self.ter.size.row - 1) {
             try self.buf.append("\r\n");
         }
--- src/main.zig
+++ src/main.zig
@@ -1,15 +1,19 @@
+const std = @import("std");
 const Terminal = @import("Terminal.zig");
 const Editor = @import("Editor.zig");
 const AppendBuffer = @import("AppendBuffer.zig");
+
+const version = "0.0.1";
 
 pub fn main() !void {
     var ter = try Terminal.enableRawMode();
     defer ter.disableRawMode();
     const buffer = AppendBuffer.init();
     defer buffer.deinit();
-    var edi = Editor.init(ter, buffer);
+    var edi = Editor.init(ter, buffer, version);
     while (true) {
         try edi.refreshScreen();
         try edi.processKeyPress();
     }
+    std.debug.print("row : {d}, col : {d}\r\n", .{ter.size.row, ter.size.col});
 }
